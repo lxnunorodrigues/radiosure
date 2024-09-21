f595eed7128546e0cdfd0c04af1304649c5803a6
lxnunorodrigues <lx.nuno.rodrigues@gmail.com>
2024-09-21 06:58:49 +0100

    Add files via upload

---
+++ radiosure.up.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+
+taskkill/im radiosure.exe
+
+cd stations
+
+del -f *.rsd
+
+certutil -urlcache -split -f "https://cld.pt/dl/download/fecd965e-645a-411d-ac80-faabbefc3f6c/latest.zip?download=true" "latest.zip"
+
+7z.exe e "latest.zip"
+
+del -f *.zip
+
+cd /d “%USERPROFILE%\Desktop”
+del RadioSure.lnk
+
+
+cd %localappdata%\RadioSure
+copy RadioSure.lnk "%USERPROFILE%\Desktop"
+Radiosure.exe
+
+exit
+exit
+
